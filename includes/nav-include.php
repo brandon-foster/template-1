751946c38d99ff998e575f6e5874c18c3eeededb
Brandon Foster <brandon.foster.vt@gmail.com>
2014-06-19 20:58:47 -0400

    rename nav-wrapper and nav to custom-nav-wrapper and custom-nav

--- includes/nav-include.php
+++ includes/nav-include.php
@@ -1,8 +1,8 @@
-<div id="nav-wrapper">
-    <ul id="nav">
+<div id="custom-nav-wrapper">
+    <ul id="custom-nav">
         <a href="index.php"><li>Welcome</li></a>
         <a href="photos.php"><li>Photos</li></a>
-        <a href="#"><li>Resume</li></a>
+        <a href="resume.php"><li>Resume</li></a>
         <a href="#"><li>Contact</li></a>
     </ul>  
 </div>
