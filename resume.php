8a854d7ee6b88f1d097dbf1635be0bd10c94e63b
Brandon Foster <brandon.foster.vt@gmail.com>
2014-06-20 23:12:51 -0400

    resume in page, and download resume button

--- resume.php
+++ resume.php
@@ -2,8 +2,8 @@
 <head>
 <meta charset="utf-8">
     <?php include"includes/links-include.php"?>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-cherry-pick.css" />
-    <title>Resume &middot; Portfolio</title>
+    <link rel="stylesheet" type="text/css" href="css/resume.css" />
+<title>Resume &middot; Portfolio</title>
 </head>
 <body>
 	<div id="container">
@@ -12,19 +12,20 @@
         <?php include"includes/nav-include.php"?>
         
         <div id="main">
-        	<div>
-        		<button class="btn"> Download PDF Resume </button>
-        	</div>
-        	<div class="content-left">
-				<?php include 'includes/content/main-resume.txt'; ?>
-        	</div>
-        	<div class="content-right">
-        		<?php include 'includes/links.php'; ?>
-        	</div>
-        </div>
+
+
+			<a href="pdfs/resume.pdf" class="btn full-width" target="_blank">Download Resume</a>
+			<br />
+			<!-- 			<br /> -->
+			<div class="iframe-resume">
+				<iframe src="pdfs/resume.pdf" frameborder="0" height="1150px"
+					width="100%">Sorry, your browser does not support iframes </iframe>
+			</div>
+		</div>
 	</div>
 
 	<!-- jQuery -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script
+		src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </body>
 </html>
