adcd5a5bdc705bc5ed141641f13cdea8325e8f83
Brandon Foster <brandon.foster.vt@gmail.com>
2014-06-21 20:43:07 -0400

    navigation includes link to contact.php

--- includes/nav-include.php
+++ includes/nav-include.php
@@ -3,7 +3,7 @@
         <a href="index.php"><li>Welcome</li></a>
         <a href="photos.php"><li>Photos</li></a>
         <a href="resume.php"><li>Resume</li></a>
-        <a href="#"><li>Contact</li></a>
+        <a href="contact.php"><li>Contact</li></a>
     </ul>  
 </div>
 
