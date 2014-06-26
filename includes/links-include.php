96101e7ca8c9b38c98beb48d1f571824b5b9172c
Brandon Foster <brandon.foster.vt@gmail.com>
2014-06-25 20:24:46 -0400

    modify navigation styles

--- includes/links-include.php
+++ includes/links-include.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" type="text/css" href="css/main.css"/>
-<link href='http://fonts.googleapis.com/css?family=Nova+Mono|Lobster' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Nova+Mono|Montserrat|Lobster' rel='stylesheet' type='text/css'>
 
