6961f11813b22ba25acbbf025077a02e93466692
Brandon Foster <brandon.foster.vt@gmail.com>
2014-06-28 13:11:45 -0400

    add href's

--- includes/nav-include.php
+++ includes/nav-include.php
@@ -1,9 +1,34 @@
-<div>
-    <ul>
-        <a href="index.php"><li>Welcome</li></a>
-        <a href="photos.php"><li>Photos</li></a>
-        <a href="resume.php"><li>Resume</li></a>
-        <a href="contact.php"><li>Contact</li></a>
-    </ul>  
-</div>
+<nav class="top-bar" data-topbar>
+	<ul class="title-area">
+		<li class="name"><h1>
+				<a href="../index.php">Template</a>
+			</h1></li>
+		<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+		<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+	</ul>
 
+	<section class="top-bar-section">
+		<!-- Right Nav Section -->
+		<ul class="right">
+			<li class="active"><a href="../index.php">Home</a></li>
+			<li><a href="../photos.php">Photos</a></li>
+			<li><a href="../resume.php">Resume</a></li>
+			<li><a href="../contact.php">Contact</a></li>
+			<li class="has-dropdown"><a href="#">Right Button Dropdown</a>
+				<ul class="dropdown">
+					<li class="has-dropdown"><a href="#">First link in dropdown</a>
+						<ul class="dropdown">
+							<li><a href="#">aaha yes</a></li>
+							<li class="has-dropdown"><a href="#">nested dropdown</a>
+								<ul class="dropdown">
+									<li><a href="#">nested inception</a></li>
+									<li><a href="#">nested inception 2</a></li>
+								</ul></li>
+							<li><a href="#">aaha yes</a></li>
+						</ul></li>
+					<li><a href="#">second links in dropdown</a></li>
+				</ul></li>
+		</ul>
+
+	</section>
+</nav>
