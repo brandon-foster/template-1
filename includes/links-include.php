2332d8ec1543dbdf51170813c2eff221f2ce4482
Brandon Foster <brandon.foster.vt@gmail.com>
2014-06-28 12:49:35 -0400

    add foundation links and modernizr script

--- includes/links-include.php
+++ includes/links-include.php
@@ -1,3 +1,7 @@
 <link rel="stylesheet" type="text/css" href="css/main.css"/>
 <link href='http://fonts.googleapis.com/css?family=Nova+Mono|Montserrat|Lobster' rel='stylesheet' type='text/css'>
 
+<link rel="stylesheet" href="foundation/css/normalize.css">
+<link rel="stylesheet" href="foundation/css/foundation.min.css">
+
+<script src="foundation/js/vendor/modernizr.js"></script>
