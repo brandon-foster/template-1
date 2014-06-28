4c787ee0210a09daf287818742dae48b2dba20c1
Brandon Foster <brandon.foster.vt@gmail.com>
2014-06-28 14:56:57 -0400

    main navigation margin-botton

--- includes/nav-include.php
+++ includes/nav-include.php
@@ -1,34 +1,36 @@
 <!-- see ../js/nav.js -->
-<nav class="top-bar" data-topbar>
-	<ul class="title-area">
-		<li class="name"><h1>
-				<a href="../index.php">Template</a>
-			</h1></li>
-		<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-		<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-	</ul>
-
-	<section class="top-bar-section">
-		<!-- Right Nav Section -->
-		<ul class="right">
-			<li id="link-index"><a href="../index.php">Home</a></li>
-			<li id="link-photos"><a href="../photos.php">Photos</a></li>
-			<li id="link-resume"><a href="../resume.php">Resume</a></li>
-			<li id="link-contact"><a href="../contact.php">Contact</a></li>
-			<li class="has-dropdown"><a href="#">Right Button Dropdown</a>
-				<ul class="dropdown">
-					<li class="has-dropdown"><a href="#">First link in dropdown</a>
-						<ul class="dropdown">
-							<li><a href="#">aaha yes</a></li>
-							<li class="has-dropdown"><a href="#">nested dropdown</a>
-								<ul class="dropdown">
-									<li><a href="#">nested inception</a></li>
-									<li><a href="#">nested inception 2</a></li>
-								</ul></li>
-							<li><a href="#">aaha yes</a></li>
-						</ul></li>
-					<li><a href="#">second links in dropdown</a></li>
-				</ul></li>
+<div clas="row" id="main-nav-row">
+	<nav class="top-bar" data-topbar>
+		<ul class="title-area">
+			<li class="name"><h1>
+					<a href="../index.php">Template</a>
+				</h1></li>
+			<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
 		</ul>
-	</section>
-</nav>
+
+		<section class="top-bar-section">
+			<!-- Right Nav Section -->
+			<ul class="right">
+				<li id="link-index"><a href="../index.php">Home</a></li>
+				<li id="link-photos"><a href="../photos.php">Photos</a></li>
+				<li id="link-resume"><a href="../resume.php">Resume</a></li>
+				<li id="link-contact"><a href="../contact.php">Contact</a></li>
+				<li class="has-dropdown"><a href="#">Right Button Dropdown</a>
+					<ul class="dropdown">
+						<li class="has-dropdown"><a href="#">First link in dropdown</a>
+							<ul class="dropdown">
+								<li><a href="#">aaha yes</a></li>
+								<li class="has-dropdown"><a href="#">nested dropdown</a>
+									<ul class="dropdown">
+										<li><a href="#">nested inception</a></li>
+										<li><a href="#">nested inception 2</a></li>
+									</ul></li>
+								<li><a href="#">aaha yes</a></li>
+							</ul></li>
+						<li><a href="#">second links in dropdown</a></li>
+					</ul></li>
+			</ul>
+		</section>
+	</nav>
+</div>
