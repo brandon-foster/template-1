bd3e05913b9f8e32afac299fc6fe8adfdf9e8f75
Brandon Foster <brandon.foster.vt@gmail.com>
2014-06-28 15:37:02 -0400

    contact.php: submission alert

--- contact.php
+++ contact.php
@@ -9,46 +9,40 @@
 <title>Contact &middot; Portfolio</title>
 </head>
 <body class="body-contact">
-	<div id="container">
-        <?php include"includes/top-bar-include.php"?>
+    <?php include"includes/top-bar-include.php"?>
         
-        <?php include"includes/nav-include.php"?>
+    <?php include"includes/nav-include.php"?>
         
-        <div id="submission-alert"></div>
+    <div id="submission-alert"><p></p></div>
 
-		<div id="main">
-			<div id="form-main">
-				<div id="form-div">
-					<form class="form" id="form1">
+	<div id="form-main">
+		<div id="form-div">
+			<form class="form" id="form1">
 
-						<p class="name">
-							<input type="text"
-								class="validate[required,custom[onlyLetter],length[0,100]] feedback-input"
-								placeholder="Name" id="name" />
-						</p>
+				<p class="name">
+					<input type="text"
+						class="validate[required,custom[onlyLetter],length[0,100]] feedback-input"
+						placeholder="Name" id="name" />
+				</p>
 
-						<p class="emailFrom">
-							<input type="text"
-								class="validate[required,custom[email-from]] feedback-input"
-								id="emailFrom" placeholder="Email" />
-						</p>
+				<p class="emailFrom">
+					<input type="text"
+						class="validate[required,custom[email-from]] feedback-input"
+						id="emailFrom" placeholder="Email" />
+				</p>
 
-						<p class="text">
-							<textarea class="validate[required,length[6,300]] feedback-input"
-								id="message" placeholder="Comment"></textarea>
-						</p>
+				<p class="text">
+					<textarea class="validate[required,length[6,300]] feedback-input"
+						id="message" placeholder="Comment"></textarea>
+				</p>
 
 
-						<div class="submit">
-							<input type="submit" value="SEND" id="button-blue" />
-							<div class="ease"></div>
-						</div>
-					</form>
+				<div class="submit">
+					<input type="submit" value="SEND" id="button-blue" />
+					<div class="ease"></div>
 				</div>
-			</div>
-
+			</form>
 		</div>
-
 	</div>
 
 
