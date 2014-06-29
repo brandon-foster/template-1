5f91300756e9154e2d35116d977dd7ec9b9abf4e
Brandon Foster <brandon.foster.vt@gmail.com>
2014-06-29 17:46:06 -0400

    add masonry for photo gallery

---
+++ js/photos.js
@@ -0,0 +1,8 @@
+<!-- Requires Masonry | visit http:// masonry.desandro.com/ to download -->
+
+$(window).load(function(){
+  $('#masonryContainer').masonry({  
+    itemSelector: '.masonry-brick',
+    columnWidth: 240
+  });  
+});
--- photos.php
+++ photos.php
@@ -10,14 +10,66 @@
 </head>
 <body class="body-photos">
 
-	<div id="container">
-        <?php include"includes/top-bar-include.php"?>
-        
-        <?php include"includes/nav-include.php"?>
-        
-        <div id="jssor-container">
-            <?php include"includes/jssor-slider-include.php"?>
-        </div>
+    <?php include"includes/top-bar-include.php"?>
+
+    <?php include"includes/nav-include.php"?>
+
+	<!-- Requires Foundation 4 | visit http://foundation.zurb.com to download -->
+
+	<!-- Masonry Container -->
+	<div class="row">
+		<div class="large-12 columns">
+			<div id="masonryContainer">
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x300&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x600&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x700&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x400&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x200&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x300&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x600&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x700&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x400&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x200&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x200&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x300&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x600&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x700&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x400&text=Item">
+				</div>
+				<div class="masonry-brick">
+					<img src="http://placehold.it/500x200&text=Item">
+				</div>
+			</div>
+		</div>
 	</div>
 
 	<!-- scripts here -->
@@ -27,6 +79,9 @@
 	<script type="text/javascript" src="js/jssor.utils.js"></script>
 	<script type="text/javascript" src="js/jssor.slider.js"></script>
 	<script type="text/javascript" src="js/jssor-slider-activate.js"></script>
+
+	<script type="text/javascript" src="masonry/masonry.pkgd.min.js"></script>
+	<script type="text/javascript" src="js/photos.js"></script>
 
 </body>
 </html>
