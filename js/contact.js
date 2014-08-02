2eeb167bce8cafa4dd61315a982a4870da4b2f9f
Brandon Foster <brandon.foster.vt@gmail.com>
2014-08-02 15:24:01 -0400

    formatting

--- js/contact.js
+++ js/contact.js
@@ -1,63 +1,63 @@
 var responseHandler = function(result) {
 
-	if (result.success) {
-		// clear fields
-		$('#name').val('');
-		$('#emailFrom').val('');
-		$('#message').val('');
+    if (result.success) {
+        // clear fields
+        $('#name').val('');
+        $('#emailFrom').val('');
+        $('#message').val('');
 
-		// show success alert
-		$('#submission-alert').removeClass('submission-failure');
-		$('#submission-alert').addClass('submission-success');
+        // show success alert
+        $('#submission-alert').removeClass('submission-failure');
+        $('#submission-alert').addClass('submission-success');
 
-		// focus on the name in case they want to send another message
-		$('#name').focus();
+        // focus on the name in case they want to send another message
+        $('#name').focus();
 
-	} else {
-		// show failure alert
-		$('#submission-alert').addClass('submission-failure');
+    } else {
+        // show failure alert
+        $('#submission-alert').addClass('submission-failure');
 
-		if (result.reason === "email") {
-			// focus on the #emailFrom field to fix their email address
-			$('#emailFrom').focus();
-		} else if (result.reason === "message") {
-			// focus on the #message field to fix their email address
-			$('#message').focus();
-		}
+        if (result.reason === "email") {
+            // focus on the #emailFrom field to fix their email address
+            $('#emailFrom').focus();
+        } else if (result.reason === "message") {
+            // focus on the #message field to fix their email address
+            $('#message').focus();
+        }
 
-	}
+    }
 
-	$('#submission-alert').html("<p>" + result.result_text + "</p>");
-	$('#submission-alert').show();
+    $('#submission-alert').html("<p>" + result.result_text + "</p>");
+    $('#submission-alert').show();
 
 }
 
 $(document).ready(function() {
-	$('#name').focus();
+    $('#name').focus();
 
-	$('a#send').click(function(event) {
+    $('a#send').click(function(event) {
 
-		var name = $('#name').val();
-		var emailFrom = $('#emailFrom').val();
-		var message = $('#message').val();
+        var name = $('#name').val();
+        var emailFrom = $('#emailFrom').val();
+        var message = $('#message').val();
 
-		// send POST request
-		$.ajax({
-			type : "POST",
-			url : "processing/process-email.php",
-			data : {
-				name : name,
-				emailFrom : emailFrom,
-				message : message,
-				submit : true
-			},
-			success : function(result) {
-				responseHandler(result);
-			},
-			dataType : "json"
-		});
+        // send POST request
+        $.ajax({
+            type : "POST",
+            url : "processing/process-email.php",
+            data : {
+                name : name,
+                emailFrom : emailFrom,
+                message : message,
+                submit : true
+            },
+            success : function(result) {
+                responseHandler(result);
+            },
+            dataType : "json"
+        });
 
-		event.preventDefault();
-	});
+        event.preventDefault();
+    });
 
 });
--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
 /* see includes/nav-include.php */
 
 $(document).ready(function() {
-	$('body.body-index li#link-index').addClass("active");
-	$('body.body-photos li#link-photos').addClass("active");
-	$('body.body-resume li#link-resume').addClass("active");
-	$('body.body-contact li#link-contact').addClass("active");
+    $('body.body-index li#link-index').addClass("active");
+    $('body.body-photos li#link-photos').addClass("active");
+    $('body.body-resume li#link-resume').addClass("active");
+    $('body.body-contact li#link-contact').addClass("active");
 });
