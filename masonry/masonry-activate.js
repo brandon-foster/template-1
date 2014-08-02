c12c1d64ab6df78e7aecd1c7a2746820bc2234e7
Brandon Foster <brandon.foster.vt@gmail.com>
2014-08-02 11:17:53 -0400

    remove masonry

--- masonry/masonry-activate.js
+++
@@ -1,8 +0,0 @@
-<!-- Requires Masonry | visit http:// masonry.desandro.com/ to download -->
-
-$(window).load(function(){
-  $('#masonryContainer').masonry({  
-    itemSelector: '.masonry-brick',
-    columnWidth: 240
-  });  
-});
