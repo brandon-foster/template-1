4dd415899beec000a342f523a9179d07986b9eaa
Brandon Foster <brandon.foster.vt@gmail.com>
2014-08-02 11:32:31 -0400

    resume page is responsive

--- resume.php
+++ resume.php
@@ -15,25 +15,29 @@
         
 
 	<div class="row">
-		<div class="small-12 columns">
-			<a href="pdfs/resume.pdf" class="button" target="_blank">Download
-				Resume</a>
-		</div>
-	</div>
+        <div class="small-12 columns">
+            <a href="pdfs/resume.pdf" class="button"
+                style="width: 100%;" target="_blank">
+                <span class="show-for-medium-up">Download Resume</span>
+                <span class="show-for-small-only">View Resume</span>
+            </a>
+        </div>
+    </div>
 
-	<div class="row">
-		<div class="small-12 columns">
-			<div class="iframe-resume">
-				<iframe src="pdfs/resume.pdf" frameborder="0" height="1150px"
-					width="100%">Sorry, your browser does not support iframes </iframe>
-			</div>
-		</div>
-	</div>
+    <div class="row show-for-medium-up">
+        <div class="large-12 columns">
+            <div class="iframe-resume">
+                <iframe src="pdfs/resume.pdf" frameborder="0"
+                    height="1150px" width="100%">Sorry, your browser
+                    does not support iframes </iframe>
+            </div>
+        </div>
+    </div>
 
-	<div class="row">&nbsp;</div>
-	<div class="row">&nbsp;</div>
+    <div class="row">&nbsp;</div>
+    <div class="row">&nbsp;</div>
 
-	<!-- scripts here -->
+    <!-- scripts here -->
 	<?php include 'includes/scripts-include.php';?>
 		
 </body>
